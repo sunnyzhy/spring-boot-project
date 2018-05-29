2194d1019218ae6c72a9e9337260f88fe5986c7f
sunnyzhy <iamaant100@163.com>
2018-05-29 23:13:18 +0800

    添加hystrix dashboard

--- gateway/src/main/java/com/zhy/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/zhy/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.zhy.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableZuulProxy
 @EnableFeignClients
 @EnableSwagger2
+@EnableHystrixDashboard
 public class GatewayApplication {
 
 	public static void main(String[] args) {
---
+++ gateway/src/main/java/com/zhy/gateway/bean/HystrixBean.java
@@ -0,0 +1,23 @@
+package com.zhy.gateway.bean;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixBean {
+    /**
+     * 因为springboot的默认路径不是 "/hystrix.stream"，所以需要添加 ServletRegistrationBean
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
